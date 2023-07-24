a8d68cf53726c47cc5599e339dc504fe9a03f5b3
ReactRestaurantMenuApp <dcitgroup37@gmail.com>
2023-07-24 14:00:49 +0000

    Screen - Profile updated 1

--- Screens/Profile.jsx
+++ Screens/Profile.jsx
@@ -1,12 +1,71 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Image } from 'react-native';
+import React from 'react';
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          style={styles.profileImage}
+          source={{ uri: 'https://via.placeholder.com/150' }}
+        />
+        <Text style={styles.name}>John Doe</Text>
+        <Text style={styles.bio}>Software Developer</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.sectionTitle}>About Me</Text>
+        <Text style={styles.sectionContent}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
+          risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec,
+          ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula
+          massa, varius a, semper congue, euismod non, mi.
+        </Text>
+        <Text style={styles.sectionTitle}>Contact Information</Text>
+        <Text style={styles.sectionContent}>
+          Email: john.doe@example.com{'\n'}
+          Phone: +1 123-456-7890{'\n'}
+          Address: 123 Main St, Anytown USA
+        </Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Profile
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  header: {
+    alignItems: 'center',
+    marginBottom: 32,
+  },
+  profileImage: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    marginBottom: 16,
+  },
+  name: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  bio: {
+    fontSize: 16,
+    color: 'gray',
+    marginBottom: 16,
+  },
+  body: {},
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  sectionContent: {
+    fontSize: 16,
+    marginBottom: 16,
+  },
+});
+
+export default Profile;
