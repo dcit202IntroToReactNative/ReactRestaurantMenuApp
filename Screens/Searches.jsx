c30074c9b18afe0f0e754e1fe81a15eaf94d1bda
coolestcode23 <coolestcode23>
2023-07-24 16:50:19 +0000

    Screen - Searches updated

--- Screens/Searches.jsx
+++ Screens/Searches.jsx
@@ -24,3 +24,23 @@ const Searches = () => {
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  searchInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingHorizontal: 8,
+    marginBottom: 16,
+  },
+});
+
+export default Searches;
