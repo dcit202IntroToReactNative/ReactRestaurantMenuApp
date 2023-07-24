b15ddf4da5dfee2f3d54a7a5465e58bd3d1ea81e
ReactRestaurantMenuApp <dcitgroup37@gmail.com>
2023-07-24 13:53:22 +0000

    Search Button Made

---
+++ Screens/Searches.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Searches = () => {
+  return (
+    <View>
+      <Text>Searches</Text>
+    </View>
+  )
+}
+
+export default Searches
