7068402baf68e198a5acf9041f55ca2eb4efc1d5
ReactRestaurantMenuApp <dcitgroup37@gmail.com>
2023-07-24 13:57:44 +0000

    Screen - Search updated 1

--- Screens/Searches.jsx
+++ Screens/Searches.jsx
@@ -1,12 +1,46 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
 
 const Searches = () => {
-  return (
-    <View>
-      <Text>Searches</Text>
-    </View>
-  )
-}
+  const [searchQuery, setSearchQuery] = useState('');
 
-export default Searches
+  const handleSearch = () => {
+    // Implement your search functionality here
+    console.log(`Searching for ${searchQuery}`);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Searches</Text>
+      <TextInput
+        style={styles.searchInput}
+        placeholder="Search..."
+        value={searchQuery}
+        onChangeText={(text) => setSearchQuery(text)}
+        onSubmitEditing={handleSearch}
+      />
+      
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  searchInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingHorizontal: 8,
+    marginBottom: 16,
+  },
+});
+
+export default Searches;
